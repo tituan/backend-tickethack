e539f74d6836626c49e2cebc708ff5669793caf4
antoine <antoine.boisard8@gmail.com>
2025-01-28 15:23:57 +0100

    regex back

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 require("../models/connection");
 require("../models/trips");
 const moment = require("moment");
+const fetch = require('node-fetch');
 
 const Trip = require("../models/trips");
 
@@ -11,27 +12,44 @@ const Trip = require("../models/trips");
 //     res.render("index", { title: "Express" });
 // });
 
+
+// This is made to search a trip with a departue arrival and date
 router.get("/trips", function (req, res) {
     let departureValue = req.body.departure
     let arrivalValue = req.body.arrival
     let dateValue = req.body.date
 
-    Trip.find()
-        .then((trips) => {
+    console.log(departureValue)
+    console.log(arrivalValue)
+    console.log(dateValue)
 
-            const matchingTrips = trips.filter(
-            (dataTrip) =>
-                dataTrip.departure.toLowerCase() === departureValue.toLowerCase() &&
-                dataTrip.arrival.toLowerCase() === arrivalValue.toLowerCase() &&
-                moment(dataTrip.date).format("YYYY-MM-DD") === moment(dateValue).format("YYYY-MM-DD")
-            )
-            // console.log(matchingTrips.length)
-
-            if (matchingTrips.length > 0) {
-                res.json({ allTrips: matchingTrips });
-            } else {
-                res.json({ message: "Aucun voyage trouvé correspondant aux critères fournis."})
+   
+    Trip.find({
+        types: {
+            $elemMatch: {
+                departure: { $regex: departureValue, $options: "i" }, 
+                arrival: { $regex: arrivalValue, $options: "i" },     
+                date: { $regex: dateValue }                        
             }
+        }
+    })
+    // departure: { $regex: "^Par", $options: "i" },
+    //   arrival: { $in: ["London", "Rome"] },
+    //   date: { $gte: "2025-01-01", $lte: "2025-12-31" } 
+        .then((trips) => {
+            console.log(trips)
+            res.json({ trips: trips });
+            // const matchingTrips = trips.filter(
+            // (dataTrip) =>
+            //     dataTrip.departure.toLowerCase() === departureValue.toLowerCase() &&
+            //     dataTrip.arrival.toLowerCase() === arrivalValue.toLowerCase() &&
+            //     moment(dataTrip.date).format("YYYY-MM-DD") === moment(dateValue).format("YYYY-MM-DD")
+            // )
+            // if (matchingTrips.length > 0) {
+            //     res.json({ allTrips: matchingTrips });
+            // } else {
+            //     res.json({ message: "Aucun voyage trouvé correspondant aux critères fournis."})
+            // }
         })
     });
 
