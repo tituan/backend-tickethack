93eb4d50a7fb292d104c8987c30dd976da9e7263
antoine <antoine.boisard8@gmail.com>
2025-01-28 13:23:42 +0100

    get trips

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 require("../models/connection");
 require("../models/trips");
+const moment = require("moment");
+
 const Trip = require("../models/trips");
 
 /* GET home page. */
@@ -10,8 +12,28 @@ const Trip = require("../models/trips");
 // });
 
 router.get("/trips", function (req, res) {
-    Trip.find().then((trips) => res.json({ allTrips: trips }));
-    // res.render("index", { title: "Express" });
-});
+    let departureValue = req.body.departure
+    let arrivalValue = req.body.arrival
+    let dateValue = req.body.date
+
+    Trip.find()
+        .then((trips) => {
+
+            const matchingTrips = trips.filter(
+            (dataTrip) =>
+                dataTrip.departure.toLowerCase() === departureValue.toLowerCase() &&
+                dataTrip.arrival.toLowerCase() === arrivalValue.toLowerCase() &&
+                moment(dataTrip.date).format("YYYY-MM-DD HH:mm:ss") === moment(dateValue).format("YYYY-MM-DD HH:mm:ss")
+            )
+            // console.log(matchingTrips.length)
+
+            if (matchingTrips.length > 0) {
+                res.json({ allTrips: matchingTrips });
+            } else {
+                res.json({ message: "Aucun voyage trouvé correspondant aux critères fournis."})
+            }
+        })
+    });
+
 
 module.exports = router;
