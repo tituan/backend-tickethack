f1a17a589c461b47eb92d902ce7efd053ce287a6
fabio <fabio.f@outlook.fr>
2025-01-29 13:17:03 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -15,71 +15,68 @@ const Cart = require("../models/cart");
 //     res.render("index", { title: "Express" });
 // });
 
-
 // This is made to search a trip with a departue arrival and date$
 
-    router.post("/", function (req, res) {
-        let departureValue = req.body.departure
-        let arrivalValue = req.body.arrival
-        let dateValue = req.body.date
-    
-        console.log(departureValue)
-        console.log(arrivalValue)
-        console.log(dateValue)
+router.post("/", function (req, res) {
+    let departureValue = req.body.departure;
+    let arrivalValue = req.body.arrival;
+    let dateValue = req.body.date;
 
-        if(!req.body.departure || !req.body.arrival || !req.body.date) {
-            res.json({ result: false, message: "Trip not found"});
+    console.log(departureValue);
+    console.log(arrivalValue);
+    console.log(dateValue);
+
+    if (!req.body.departure || !req.body.arrival || !req.body.date) {
+        res.json({ result: false, message: "Trip not found" });
+    }
+
+    Trip.find({
+        departure: { $regex: new RegExp(req.body.departure, "i") },
+        arrival: { $regex: new RegExp(req.body.arrival, "i") },
+        date: {
+            $gte: moment.utc(req.body.date).startOf("day").toDate(),
+            $lt: moment.utc(req.body.date).endOf("day").toDate(),
+        },
+    }).then((trips) => {
+        if (trips.length === 0) {
+            res.json({ result: false, message: "Trip not found" });
+        } else {
+            res.json({ result: true, trips });
         }
-       
-        Trip.find({ departure: { $regex: new RegExp(req.body.departure, 'i') }, 
-                    arrival: { $regex: new RegExp(req.body.arrival, 'i') }, 
-                    date: { 
-                        $gte: moment.utc(req.body.date).startOf("day").toDate(), 
-                        $lt: moment.utc(req.body.date).endOf("day").toDate() 
-                        } 
-                    })
-            .then((trips) => {
-                if (trips.length === 0) {
-                    res.json({ result: false, message: "Trip not found"});
-                } else {
-                    res.json({ result: true, trips});
-                }
-            })
     });
+});
 
-    router.post("/cart", function (req, res) {
-        
-        console.log(req.body.date)
-        
-        Cart.find({ 
-                departure: req.body.departure,
-                // arrival: req.body.arrival,
-                // date: req.body.date 
-                })
-        .then(data => {
-            console.log(data)
+router.post("/cart", function (req, res) {
+    console.log(req.body.date);
+
+    Cart.find({
+        departure: req.body.departure,
+        arrival: req.body.arrival,
+        date: req.body.date,
+    }).then(
+        (data) => {
+            // console.log(data);
             // console.log(req.body.departure)
             // console.log(req.body.arrival)
-            // console.log(req.body.date)
-            // if (data) {
-            //     res.json({ result: false, message: "Trip exist"});
-            // } else {
-                const newCart = new Cart ({
+            console.log(req.body.date);
+            if (data) {
+                res.json({ result: false, message: "Trip exist" });
+            } else {
+                const newCart = new Cart({
                     departure: req.body.departure,
                     arrival: req.body.arrival,
                     date: req.body.date,
                     price: req.body.price,
                 });
-        
-                newCart.save().then( newTrip => { 
-                    res.json({ result: true, newTrip});
+
+                newCart.save().then((newTrip) => {
+                    res.json({ result: true, newTrip });
                 });
-                
-            // }
-        })
-        
-           
+            }
+        }
+
         // res.json({ result: true });
-    });
+    );
+});
 
 module.exports = router;
